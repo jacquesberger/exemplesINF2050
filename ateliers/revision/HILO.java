356c2209770c5722e5febccec2ca027e2c3c750b
Jacques Berger <jacques.berger@code3.ca>
2020-07-07 11:22:59 -0400

    Quelques changements à un exemple

--- ateliers/revision/HILO.java
+++ ateliers/revision/HILO.java
@@ -21,7 +21,7 @@
                 int nbrTentatives= 1;
                 do{
 
-                    if ((nbrEntre >= 0) && (nbrEntre <= 100)) {
+                    if (( nbrEntre >= 0 ) && (nbrEntre <= 100)) {
 
                         if (nbrEntre>nbrMystere){
                             System.out.println ("Plus bas");
@@ -31,19 +31,19 @@
                     }
 
                     else {
-                        System.out.println ("le nombre doit etre entre 0 et 100 inclusivement! Veuillez etrer un nouveau nombre:");
+                        System.out.println ("Le nombre doit etre entre 0 et 100 inclusivement! Veuillez etrer un nouveau nombre:");
                     }
                     nbrEntre= input.nextInt();
                     if (nbrEntre==nbrMystere){
-                        System.out.println ("Bravo vous avez gagné");
+                        System.out.println ("Bravo vous avez gagner");
                         partiesGagnees = partiesGagnees+partiesGagnees;
                     }else if (nbrEntre!=nbrMystere && nbrTentatives==10){
-                        System.out.println ("Désolé! Vous avez perdu");}
+                        System.out.println ("désolé! Vous avez perdu");}
 
                 }while (nbrEntre!=nbrMystere && nbrTentatives<10);
                 partiesJouees = partiesJouees+partiesJouees;
                 do{
-                    System.out.println("Voulez-vous jouer une nouvelle partie?(oui/non)");
+                    System.out.println("Voulez-vous jouer une nouvelle partie? (oui/non)");
                     decision = input.next();
                     if (decision.equalsIgnoreCase("non")){
                         resultat = "Vous avez joué " +partiesJouees +" partie";
@@ -56,12 +56,12 @@
                         String ch2= "sur les" + partiesJouees+ "parties jouées.";
                         if (partiesGagnees>1){
                             ch1 = ch1 + "s";}
-                        ch2 = " sur la seule partie que vous avez jouée.";
+                        ch2 = " sur la seule partie que vous avez joué.";
 
                     annonce = ch1+ch2;
                     System.out.println (annonce);
                     tauxReussite = (partiesGagnees*100)/partiesJouees;
-                    System.out.println ("Votre taux de succs est: " +tauxReussite+ "%");
+                    System.out.println ("Votre taux de succès est: " +tauxReussite+ "%");
                 }
 
             }while(!decision.equalsIgnoreCase ("non") && !decision.equalsIgnoreCase ("oui"));
